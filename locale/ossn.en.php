b0960bbea64e80599fc6385824196f4674084ffa
Anupama Upadhyayula <38144301+anupama2501@users.noreply.github.com>
2018-07-10 17:41:53 -0700

    Create ossn.en.php

---
+++ locale/ossn.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+	'dashb' => 'Dashb',
+);
+ossn_register_languages('en', $en); 
